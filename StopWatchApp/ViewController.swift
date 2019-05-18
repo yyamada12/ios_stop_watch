86749c5e1b4fad895d13d9d7753e47c8c2a3b903
yyamada12 <12yacropolisy@gmail.com>
2019-05-19 08:11:21 +0900

    [add] function of start button

--- StopWatchApp/ViewController.swift
+++ StopWatchApp/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    var startTime: TimeInterval? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @objc func update() {
+        if let startTime = self.startTime {
+            let t: Double = Date.timeIntervalSinceReferenceDate - startTime
+            let min = Int(t / 60)
+            let sec = Int(t) % 60
+            let msec = Int((t - Double(min * 60) - Double(sec)) * 100.0)
+            self.timerLabel.text = String(format: "%02d:%02d:%02d", min, sec, msec)
+        }
+        
+    }
 
-
+    @IBAction func startTimer(_ sender: Any) {
+        self.startTime = Date.timeIntervalSinceReferenceDate
+        Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func stopTimer(_ sender: Any) {
+    }
+    
+    @IBAction func resetTimer(_ sender: Any) {
+    }
 }
 
